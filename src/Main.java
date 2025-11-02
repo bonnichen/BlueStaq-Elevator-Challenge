77134c88048a729c144e831c06f050aa7f44f3a6
bonnichen <bonnie.chen1208@gmail.com>
2025-11-02 02:31:15 -0500

    Implemented a simulation simulating elevator functions:
    - Added Passenger class with origin and destination
    - Added Floor class managing waiting passengers
    - Added Elevator class handling movement between floors, passenger boarding, and passenger exiting

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
+import model.Simulation;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Simulation simulation = new Simulation(10);
+        simulation.setUpFloor();
+        simulation.createPassengers(0,9);
+        simulation.createPassengers(0,1);
+        simulation.createPassengers(0,8);
+        simulation.createPassengers(0,3);
+        simulation.createPassengers(0,1);
+        simulation.createPassengers(0,6);
+        simulation.createPassengers(0,7);
+        simulation.setUpPassenger();
+        simulation.setUpElevator();
+        simulation.runElevator();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
